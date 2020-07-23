6c20bf38cc9dff36aecdfa87bead87a603bee0be
Darren Benson <darren.benson@deskpoint.com>
2020-07-23 23:02:52 +0100

    Add force on collision or hit

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -5,11 +5,29 @@ public class Explosion : MonoBehaviour
 {
 
     [SerializeField] private GameObject explosion;
+    [SerializeField] private Rigidbody rigidBody;
+    [SerializeField] private float _laserHitForce = 10f;
 
     public void OnHit(Vector3 pos)
     {
         GameObject myExplosion = Instantiate(explosion, pos, Quaternion.identity, transform);
         Destroy(myExplosion, 6f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach(var contactPoint in collision.contacts)
+        {
+            OnHit(contactPoint.point);
+        }
+    }
+
+    public void AddForce(Vector3 hitPosition, Transform hitSource)
+    {
+        Debug.LogWarning("AddForce: " + gameObject.name + " -> " + hitSource.name);
+        if (rigidBody == null) return;
+        Vector3 forceDirection = (hitSource.position - transform.position).normalized;
+        rigidBody.AddForceAtPosition(forceDirection * _laserHitForce, hitPosition, ForceMode.Impulse);
     }
 
 }
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -63,7 +63,11 @@ public class Laser : MonoBehaviour
     {
         Explosion hitExplosion = target.GetComponent<Explosion>();
         if (hitExplosion != null)
+        {
             hitExplosion.OnHit(hitPosition);
+            hitExplosion.AddForce(hitPosition, transform);
+        }
+            
     }
 
     public void FireLaser() 
