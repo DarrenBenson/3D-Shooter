2c18b0f5f763eced92eaf0981e9a024e44f25792
Darren Benson <darren.benson@deskpoint.com>
2020-07-23 01:15:01 +0100

    Removed Mouse control and switched to keyboard only

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -30,8 +30,8 @@ public class PlayerMovement : MonoBehaviour {
     private void Turn()
     {
         float yaw = _turnSpeed * Time.deltaTime * CrossPlatformInputManager.GetAxis("Horizontal");
-        float pitch = _turnSpeed * Time.deltaTime * -CrossPlatformInputManager.GetAxis("Mouse Y");
-        float roll = _turnSpeed * Time.deltaTime * -CrossPlatformInputManager.GetAxis("Mouse X");
+        float pitch = _turnSpeed * Time.deltaTime * -CrossPlatformInputManager.GetAxis("Pitch");
+        float roll = _turnSpeed * Time.deltaTime * -CrossPlatformInputManager.GetAxis("Roll");
         TransformCache.Rotate(pitch, yaw, roll);
     }
 
